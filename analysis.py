e4d05d3800fc356ec3f92a1a30d119531e20e691
ShanAmjad <MckayKonieczny3481522@outlook.com>
2017-02-28 15:20:11 +0100

    Updated README with new features

--- analysis.py
+++ analysis.py
@@ -3,3 +3,4 @@ Temporary commit content for 2017-02-09T15:20:11
 Temporary commit content for 2017-02-11T15:20:11
 Temporary commit content for 2017-02-23T15:20:11
 Temporary commit content for 2017-02-25T15:20:11
+Temporary commit content for 2017-02-28T15:20:11
