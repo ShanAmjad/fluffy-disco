8e7a4385ec346a9ba57c61004ac3d49de86d5428
ShanAmjad <MckayKonieczny3481522@outlook.com>
2017-03-17 15:20:11 +0100

    Optimized database queries

--- script.ps1
+++ script.ps1
@@ -2,3 +2,4 @@ Initial content for script.ps1
 Temporary commit content for 2017-02-03T15:20:11
 Temporary commit content for 2017-02-04T15:20:11
 Temporary commit content for 2017-03-12T15:20:11
+Temporary commit content for 2017-03-17T15:20:11
