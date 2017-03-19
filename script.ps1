5160326a2e14eeb61f37e757b7049e49150f681f
ShanAmjad <MckayKonieczny3481522@outlook.com>
2017-03-19 15:20:11 +0100

    Updated project documentation

--- script.ps1
+++ script.ps1
@@ -3,3 +3,4 @@ Temporary commit content for 2017-02-03T15:20:11
 Temporary commit content for 2017-02-04T15:20:11
 Temporary commit content for 2017-03-12T15:20:11
 Temporary commit content for 2017-03-17T15:20:11
+Temporary commit content for 2017-03-19T15:20:11
