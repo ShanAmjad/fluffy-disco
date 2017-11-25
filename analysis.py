1b1774d342c810815ec1933227a7f5b9149843ea
ShanAmjad <MckayKonieczny3481522@outlook.com>
2017-11-25 15:20:11 +0100

    Optimized database queries

--- analysis.py
+++ analysis.py
@@ -22,3 +22,4 @@ Temporary commit content for 2017-09-02T15:20:11
 Temporary commit content for 2017-10-11T15:20:11
 Temporary commit content for 2017-10-28T15:20:11
 Temporary commit content for 2017-11-11T15:20:11
+Temporary commit content for 2017-11-25T15:20:11
