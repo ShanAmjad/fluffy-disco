a05713ef277f3bd92420ccdc627e9bd37f50af81
ShanAmjad <MckayKonieczny3481522@outlook.com>
2018-06-14 15:20:11 +0100

    Fixed typos in documentation

--- analysis.py
+++ analysis.py
@@ -37,3 +37,4 @@ Temporary commit content for 2018-05-05T15:20:11
 Temporary commit content for 2018-05-06T15:20:11
 Temporary commit content for 2018-05-07T15:20:11
 Temporary commit content for 2018-05-13T15:20:11
+Temporary commit content for 2018-06-14T15:20:11
