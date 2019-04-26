c08974715ed98cf28667bbf1b81e6a4b2f35c4d1
ShanAmjad <MckayKonieczny3481522@outlook.com>
2019-04-26 15:20:11 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -83,3 +83,4 @@ Temporary commit content for 2019-02-16T15:20:11
 Temporary commit content for 2019-02-18T15:20:11
 Temporary commit content for 2019-03-01T15:20:11
 Temporary commit content for 2019-04-18T15:20:11
+Temporary commit content for 2019-04-26T15:20:11
