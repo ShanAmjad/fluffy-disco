4ec9085b160247bc5c87039ea693f5c7e24dabe3
ShanAmjad <MckayKonieczny3481522@outlook.com>
2020-05-09 15:20:11 +0100

    Improved error handling logic

--- analysis.py
+++ analysis.py
@@ -98,3 +98,4 @@ Temporary commit content for 2020-04-03T15:20:11
 Temporary commit content for 2020-04-05T15:20:11
 Temporary commit content for 2020-04-10T15:20:11
 Temporary commit content for 2020-04-12T15:20:11
+Temporary commit content for 2020-05-09T15:20:11
