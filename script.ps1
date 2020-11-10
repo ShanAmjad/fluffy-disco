2c4d5049c35ebbf185677dde0246455b1ab2ae6f
ShanAmjad <MckayKonieczny3481522@outlook.com>
2020-11-10 15:20:11 +0100

    Improved error handling logic

--- script.ps1
+++ script.ps1
@@ -130,3 +130,4 @@ Temporary commit content for 2020-10-04T15:20:11
 Temporary commit content for 2020-10-17T15:20:11
 Temporary commit content for 2020-10-18T15:20:11
 Temporary commit content for 2020-10-30T15:20:11
+Temporary commit content for 2020-11-10T15:20:11
