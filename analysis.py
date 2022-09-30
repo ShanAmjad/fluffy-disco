0030952f17a66fb546024f7f1739230f7f4a490c
ShanAmjad <MckayKonieczny3481522@outlook.com>
2022-09-30 15:20:11 +0100

    Refactored code for better readability

--- analysis.py
+++ analysis.py
@@ -172,3 +172,4 @@ Temporary commit content for 2022-09-13T15:20:11
 Temporary commit content for 2022-09-17T15:20:11
 Temporary commit content for 2022-09-27T15:20:11
 Temporary commit content for 2022-09-28T15:20:11
+Temporary commit content for 2022-09-30T15:20:11
