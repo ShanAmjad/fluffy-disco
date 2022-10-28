a6bdec83d63a7f329af217ceb907f19a239d71b7
ShanAmjad <MckayKonieczny3481522@outlook.com>
2022-10-28 15:20:11 +0100

    Updated README with new features

--- script.ps1
+++ script.ps1
@@ -184,3 +184,4 @@ Temporary commit content for 2022-09-09T15:20:11
 Temporary commit content for 2022-09-22T15:20:11
 Temporary commit content for 2022-10-14T15:20:11
 Temporary commit content for 2022-10-16T15:20:11
+Temporary commit content for 2022-10-28T15:20:11
