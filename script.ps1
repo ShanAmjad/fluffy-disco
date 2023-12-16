32c26b3dbfd7756c32bf9433966e0304bd480c66
ShanAmjad <MckayKonieczny3481522@outlook.com>
2023-12-16 15:20:11 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -217,3 +217,4 @@ Temporary commit content for 2023-10-11T15:20:11
 Temporary commit content for 2023-11-21T15:20:11
 Temporary commit content for 2023-12-08T15:20:11
 Temporary commit content for 2023-12-14T15:20:11
+Temporary commit content for 2023-12-16T15:20:11
