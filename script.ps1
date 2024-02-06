2f7849f1bcd1bfd8a8ebe8f0a142a28597b8aca3
ShanAmjad <MckayKonieczny3481522@outlook.com>
2024-02-06 15:20:11 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -226,3 +226,4 @@ Temporary commit content for 2024-01-12T15:20:11
 Temporary commit content for 2024-01-15T15:20:11
 Temporary commit content for 2024-01-20T15:20:11
 Temporary commit content for 2024-02-03T15:20:11
+Temporary commit content for 2024-02-06T15:20:11
