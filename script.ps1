b80cb5684f641ee15c9251a8efc274beb95fa573
ShanAmjad <MckayKonieczny3481522@outlook.com>
2024-05-07 15:20:11 +0100

    Enhanced UI responsiveness

--- script.ps1
+++ script.ps1
@@ -229,3 +229,4 @@ Temporary commit content for 2024-02-03T15:20:11
 Temporary commit content for 2024-02-06T15:20:11
 Temporary commit content for 2024-02-10T15:20:11
 Temporary commit content for 2024-04-26T15:20:11
+Temporary commit content for 2024-05-07T15:20:11
