398805644ab929fddb929b776445000ce8343c99
ShanAmjad <MckayKonieczny3481522@outlook.com>
2024-05-23 15:20:11 +0100

    Fixed typos in documentation

--- analysis.py
+++ analysis.py
@@ -229,3 +229,4 @@ Temporary commit content for 2024-03-29T15:20:11
 Temporary commit content for 2024-04-07T15:20:11
 Temporary commit content for 2024-04-16T15:20:11
 Temporary commit content for 2024-05-21T15:20:11
+Temporary commit content for 2024-05-23T15:20:11
